8f4ffad721dd477586fb4dd7d1c6f6fd19f6791d
Haris Sekar <sharis14003@gmail.com>
2022-07-23 14:30:56 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ import cors from "cors";
 const app = express();
 dotenv.config();
 
+app.use(express.json({limit:"30mb", extended:"true"}));
+app.use(express.urlencoded({limit:"30mb", extended:"true"}));
+app.use(cors()); 
+
+console.log("handling routes");
 app.get('/',(req,res)=>{
     res.send('Hello World');
 })
@@ -14,3 +19,7 @@ app.get('/',(req,res)=>{
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
 })
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser : true, useUnifiedTopology: true})
+    .then( () => app.listen(PORT,() => console.log(`Server running on port:${PORT}`)))
+    .catch( (error) => console.log(error.message));
